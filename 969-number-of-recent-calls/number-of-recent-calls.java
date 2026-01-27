2222ab1b5110a14c712f970882189dacdd9ff4c9
Amit prajapati <amitkumar2020grl@gmail.com>
2026-01-28 00:24:03 +0530

    Time: 22 ms (56.66%) | Memory: 60.1 MB (8.25%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.java
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -1,20 +1,47 @@
+//Queue method
 class RecentCounter {
-    List<Integer> list;
+    Queue<Integer> q;
 
-    public RecentCounter() {
-        list = new ArrayList<>();
-        
+    public RecentCounter(){
+        q = new LinkedList<>();
+
     }
-    
     public int ping(int t) {
-        list.add(t);
-        while(list.get(0) < t-3000){
-            list.remove(0);
+        q.add(t);
+
+        while(q.peek()< t-3000){
+            q.remove();
         }
-        return list.size();
-  
+        return q.size();
     }
+
+
 }
+
+
+
+
+
+
+// class RecentCounter {
+//     List<Integer> list;
+
+//     public RecentCounter() {
+//         list = new ArrayList<>();
+        
+//     }
+    
+//     public int ping(int t) {
+//         list.add(t);
+
+//         //Sliding Window
+//         while(list.get(0) < t-3000){
+//             list.remove(0);
+//         }
+//         return list.size();
+  
+//     }
+// }
 
 /**
  * Your RecentCounter object will be instantiated and called as such:
