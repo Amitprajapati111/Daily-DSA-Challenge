32f2610859cf2be1e9ef3bd2b70f0e57bf865c92
Amit prajapati <amitkumar2020grl@gmail.com>
2026-02-15 14:54:02 +0530

    Time: 3 ms (87.46%) | Memory: 47.9 MB (32.31%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        //
+        int[] candies = new int[n];
+
+        // Step:1 Sabhi ko 1-1 candies de do
+        for(int i=0; i<n; i++){
+            candies[i]=1;
+        }
+
+
+        // Step:2 Left to Right
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+
+        //Step:3 Right to Left
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1]+1);
+            }
+        }
+
+        //Step:4 Sum of all
+        int total = 0;
+        for(int c:candies){
+            total += c;
+        }        
+        return total;
+    }
+}
