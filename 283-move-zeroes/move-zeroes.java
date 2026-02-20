31736e98f3a0eab1a74f467c28c5c8944ee3dbb7
Amit prajapati <amitkumar2020grl@gmail.com>
2026-02-20 18:43:39 +0530

    Time: 1 ms (99.76%) | Memory: 47.6 MB (93.08%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                nums[pos] = nums[i];
+                pos++;
+            }
+
+        }
+        for(int i=pos; i<=n-1; i++){
+            nums[i] = 0;
+        }
+    }
+}
